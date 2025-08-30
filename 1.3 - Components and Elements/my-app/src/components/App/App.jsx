d8fa9171c82c48d0263dba591f12e372d9403b8f
Miryoqib <miryoqibcoder@gmail.com>
2025-08-30 18:46:10 +0500

    1.3 - Компоненты и элементы

---
+++ 1.3 - Components and Elements/my-app/src/components/App/App.jsx
@@ -0,0 +1,13 @@
+import { MyName } from '../MyName/MyName';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div><h1>Привет, React</h1></div>
+      <MyName />
+    </>
+  )
+};
+
+export default App
---
+++ 1.3 - Components and Elements/my-app/src/components/MyName/MyName.jsx
@@ -0,0 +1,7 @@
+import "./MyName.css";
+
+export function MyName() {
+    return (
+        <div><h2>Меня зовут, Мирёкуб</h2></div>
+    )
+};
