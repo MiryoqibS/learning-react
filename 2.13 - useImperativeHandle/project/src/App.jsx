6273f5ac17d048b62c9fa5c51acd44308429d58d
Miryoqib <miryoqibcoder@gmail.com>
2025-09-22 18:01:37 +0500

    2.13 – Хук useImperativeHandle

---
+++ 2.13 - useImperativeHandle/project/src/App.jsx
@@ -0,0 +1,21 @@
+import React, { useRef } from 'react'
+import { Modal } from './components/Modal';
+
+export const App = () => {
+  const modalRef = useRef(null);
+  return (
+    <div className="container mx-auto flex flex-col items-center gap-8">
+      <Modal ref={modalRef} />
+      <div className="flex items-center gap-4">
+        <button
+          className="px-4 py-2 rounded bg-blue-600 text-white font-semibold cursor-pointer transition-colors hover:bg-blue-800"
+          onClick={() => modalRef.current.open()}
+        >Открыть</button>
+        <button
+          className="px-4 py-2 rounded bg-blue-600 text-white font-semibold cursor-pointer transition-colors hover:bg-blue-800"
+          onClick={() => modalRef.current.close()}
+        >Закрыт</button>
+      </div>
+    </div>
+  )
+}
---
+++ 2.13 - useImperativeHandle/project/src/components/Modal.jsx
@@ -0,0 +1,25 @@
+import React, { forwardRef, useImperativeHandle, useRef } from 'react';
+
+export const Modal = forwardRef((props, ref) => {
+    const modalRef = useRef(null);
+
+    useImperativeHandle(ref, () => ({
+        open() {
+            modalRef.current.classList.remove("opacity-0");
+            modalRef.current.classList.add("opacity-100");
+        },
+        close() {
+            modalRef.current.classList.remove("opacity-100");
+            modalRef.current.classList.add("opacity-0");
+        },
+    }));
+
+    return (
+        <div
+            ref={modalRef}
+            className="bg-white border-2 border-gray-300 shadow-lg rounded-xl flex items-center justify-center px-4 py-4 w-[300px] mx-auto mt-6 opacity-0 transition-opacity duration-300"
+        >
+            <h3 className="text-xl font-semibold">Модальное окно</h3>
+        </div>
+    );
+});
